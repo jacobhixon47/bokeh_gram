6fc723c6bb073ec0b487d68a3def9f40db327564
Jacob Hixon and Skye Atwood <jacob@>
2017-02-15 16:09:51 -0800

    bugs: only displays thumbnail for images#new on initial load. document technically only loads once. no solutions so far

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
-  has_attached_file :picture, :styles => {:large => "800x800", :medium => "300x300>", :thumb => "100x100" }
+  has_attached_file :picture, :styles => { :large => "800x800>", :medium => "500x500>", :thumb => "100x100" }
 
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
