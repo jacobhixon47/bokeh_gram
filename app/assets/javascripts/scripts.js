cf38eb9e418617246c076f66b0594f45b8dddc80
Jacob Hixon and Skye Atwood <skye@>
2017-02-15 16:45:43 -0800

    get isotope image display working finally. And thumbnail when uploading.

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,17 +1,23 @@
-// $(document).ready(function() {
-//   $(function() {
-//     $('#pictureInput').on('change', function(event) {
-//       var files = event.target.files;
-//       var image = files[0]
-//       var reader = new FileReader();
-//       reader.onload = function(file) {
-//         var img = new Image();
-//         console.log(file);
-//         img.src = file.target.result;
-//         $('#target').html(img);
-//       }
-//       reader.readAsDataURL(image);
-//       console.log(files);
-//     });
-//   });
-// });
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    $('#pictureInput').on('change', function(event) {
+      var files = event.target.files;
+      var image = files[0]
+      var reader = new FileReader();
+      reader.onload = function(file) {
+        var img = new Image();
+        console.log(file);
+        img.src = file.target.result;
+        $('#target').html(img);
+      }
+      reader.readAsDataURL(image);
+      console.log(files);
+    });
+  });
+
+  $(function() {
+    $('.grid').isotope({
+      itemSelector: '.grid-item'
+    });
+  });
+});
